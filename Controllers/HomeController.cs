a2d84c5ffd92f4abf1b5ef139c9d3277077d308b
ellieyeonhee <155503070+ellieyeonhee@users.noreply.github.com>
2024-09-19 10:40:48 +1000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auctions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.active = "Index";
+            return View();
+        }
+
+        public ActionResult Catalogue()
+        {
+            ViewBag.active = "Index";
+            return View();
+        }
+
+        public ActionResult Lot()
+        {
+            ViewBag.active = "Index";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.active = "About";
+            ViewBag.Message = "DDWT Auction page";
+            return View();
+        }
+
+
+    }
+}
